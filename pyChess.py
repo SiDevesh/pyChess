54e29bd9ad0ace42f17807439172d7c2a142244d
SiDevesh <sid.swapnildevesh@gmail.com>
2016-06-10 00:33:42 +0530

    Board matrix is now of pieces objects Added initial player and game classes

--- pyChess.py
+++ pyChess.py
@@ -9,8 +9,8 @@ class ChessBoard:
 		assert (((width<8)or(height<5)) == False),"Insufficient board size for chess"
 		self.width = width
 		self.height = height
-		self.board_matrix = [[0 for x in range(width)] for y in range(height)]
-
+		self.board_matrix = [[None for x in range(width)] for y in range(height)]
+		self.deadpool = [None for x in range((width)*(height))]
 		#Representation of pieces:
 		#  PW-Pawn White
 		#  KW-King White
@@ -26,12 +26,58 @@ class ChessBoard:
 		#  NB-Knight Black
 		#  RB-Rook Black
 
-		print_ordered_pieces = [{'role':'R', 'color':'B'}, {'role':'K', 'color':'B'}, {'role':'B', 'color':'B'}, {'role':'Q', 'color':'B'}, {'role':'K', 'color':'B'}, {'role':'B', 'color':'B'}, {'role':'K', 'color':'B'}, {'role':'R', 'color':'B'}, {'role':'P', 'color':'B'}, {'role':'P', 'color':'B'}, {'role':'P', 'color':'B'}, {'role':'P', 'color':'B'}, {'role':'P', 'color':'B'}, {'role':'P', 'color':'B'}, {'role':'P', 'color':'B'}, {'role':'P', 'color':'B'}, {'role':'P', 'color':'W'}, {'role':'P', 'color':'W'}, {'role':'P', 'color':'W'}, {'role':'P', 'color':'W'}, {'role':'P', 'color':'W'}, {'role':'P', 'color':'W'}, {'role':'P', 'color':'W'}, {'role':'P', 'color':'W'}, {'role':'R', 'color':'W'}, {'role':'K', 'color':'W'}, {'role':'B', 'color':'W'}, {'role':'Q', 'color':'B'}, {'role':'K', 'color':'W'}, {'role':'B', 'color':'W'}, {'role':'K', 'color':'W'}, {'role':'R', 'color':'W'} ]
+		pc_pawn pawnW1
+		pc_pawn pawnW2
+		pc_pawn pawnW3
+		pc_pawn pawnW4
+		pc_pawn pawnW5
+		pc_pawn pawnW6
+		pc_pawn pawnW7
+		pc_pawn pawnW8
+
+		pc_rook rookW1
+		pc_rook rookW2
+
+		pc_bishop bishopW1
+		pc_bishop bishopW2
+
+		pc_knight knightW1
+		pc_knight knightW2
+
+		pc_king kingW
+
+		pc_queen queenW
+
+		pc_pawn pawnB1
+		pc_pawn pawnB2
+		pc_pawn pawnB3
+		pc_pawn pawnB4
+		pc_pawn pawnB5
+		pc_pawn pawnB6
+		pc_pawn pawnB7
+		pc_pawn pawnB8
+
+		pc_rook rookB1
+		pc_rook rookB2
+
+		pc_bishop bishopB1
+		pc_bishop bishopB2
+
+		pc_knight knightB1
+		pc_knight knightB2
+
+		pc_king kingB
+					
+		pc_queen queenB
+
+		#print_ordered_pieces = [{'role':'R', 'color':'B'}, {'role':'K', 'color':'B'}, {'role':'B', 'color':'B'}, {'role':'Q', 'color':'B'}, {'role':'K', 'color':'B'}, {'role':'B', 'color':'B'}, {'role':'K', 'color':'B'}, {'role':'R', 'color':'B'}, {'role':'P', 'color':'B'}, {'role':'P', 'color':'B'}, {'role':'P', 'color':'B'}, {'role':'P', 'color':'B'}, {'role':'P', 'color':'B'}, {'role':'P', 'color':'B'}, {'role':'P', 'color':'B'}, {'role':'P', 'color':'B'}, {'role':'P', 'color':'W'}, {'role':'P', 'color':'W'}, {'role':'P', 'color':'W'}, {'role':'P', 'color':'W'}, {'role':'P', 'color':'W'}, {'role':'P', 'color':'W'}, {'role':'P', 'color':'W'}, {'role':'P', 'color':'W'}, {'role':'R', 'color':'W'}, {'role':'K', 'color':'W'}, {'role':'B', 'color':'W'}, {'role':'Q', 'color':'B'}, {'role':'K', 'color':'W'}, {'role':'B', 'color':'W'}, {'role':'K', 'color':'W'}, {'role':'R', 'color':'W'} ]
+		print_ordered_pieces = [rookB2, knightB2, bishopB2, queenB, kingB, bishopB1, knightB1, rookB1, pawnB8, pawnB7, pawnB6, pawnB5, pawnB4, pawnB3, pawnB2, pawnB1,  pawnW1, pawnW2, pawnW3, pawnW4, pawnW5, pawnW6, pawnW7, pawnW1, rookW1, knightW1, bishopW1, queenW, kingW, bishopW2, knightW2, rookW2 ]
 		pc_i = 0
 		for i in range(height):
 			for j in range(width):
 				if((i<2)or(i>=(height-2))):
 					#if width of board is more than 8, extra spaces are put empty on left
+					
 					if(j<8):
 						self.board_matrix[i][j] = print_ordered_pieces[pc_i]
 						pc_i+=1	
@@ -42,7 +88,8 @@ class ChessBoard:
 
 	def board_print(self):
 		for i in range(self.height):
-			print self.board_matrix[i]
+			for j in range(self.width):
+				print self.board_matrix[i][j].symbol
 
 class pc_rook:
 	symbol = 'R'
@@ -331,13 +378,44 @@ def move_pc(board, init_pos, final_pos, color):
 	assert (final_pos in any_pc_moves_pos(board.board_matrix[init_pos['x']][init_pos['y']]['role'], board, init_pos, color)),"Invalid move for the selected piece"
 	if(board.board_matrix[final_pos['x']][final_pos['y']]['role']!='E'):
 		dead_pc = board.board_matrix[final_pos['x']][final_pos['y']]
+		board.board_matrix[final_pos['x']][final_pos['y']] = board.board_matrix[init_pos['x']][init_pos['y']]
+		board.board_matrix[init_pos['x']][init_pos['y']] = {'role':'E', 'color':'E'}
+		board.deadpool.append(dead_pc)
+		return
 	else:
 		dead_pc = {'role':'E', 'color':'E'}
-	board.board_matrix[final_pos['x']][final_pos['y']] = board.board_matrix[init_pos['x']][init_pos['y']]
-	board.board_matrix[init_pos['x']][init_pos['y']] = {'role':'E', 'color':'E'}
-	return dead_pc
+		board.board_matrix[final_pos['x']][final_pos['y']] = board.board_matrix[init_pos['x']][init_pos['y']]
+		board.board_matrix[init_pos['x']][init_pos['y']] = {'role':'E', 'color':'E'}
+		return
 	
+
+
+#implement castling and first mov two pawn can move
+
+
+
+#till here
+
+
+class player:
+	name = 'NoName'
+	color = 'E'
+	isAI = False
+	movesList = [{'pc':'null', 'init_pos':'null', 'final_pos':'null', 'desc':'null'}]
+	def __init__(name, color, isAI):
+		self.name = name
+		self.color = color
+		self.isAI = isAI
+
+
+class game:
+	def __init__(self, ChessBoard, player1, player2):
+
+
+
 
 
 first = ChessBoard(8,8)
 first.board_print()
+
+#I updated the array with pieces object rather than {'role':'E', 'color':'E'} type thingy, make respective changes in the pieces's moves_pos function and also in the move_pc function
